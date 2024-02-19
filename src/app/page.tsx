20d638c9c7bdbddacf83c54169cd511efab23d1e
Marcin Jaborski <marcin.jaborski@gmail.com>
2024-02-19 23:18:09 +0100

    Connect supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <h1 className="text-blue-400">Expense tracker</h1>;
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export default async function Home() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data } = await supabase.from("expenses").select();
+
+  return (
+    <ul>{data?.map((todo) => <li key={todo.id}>{todo.description}</li>)}</ul>
+  );
 }
