204d3fc8e8ce502d73d48079249f71d0d36c02c2
Marcin Jaborski <marcin.jaborski@gmail.com>
2024-05-22 20:05:45 +0200

    Add total money to dashboard

--- src/app/[locale]/(logged)/page.tsx
+++ src/app/[locale]/(logged)/page.tsx
@@ -5,6 +5,7 @@ import { getMessages, getTranslations } from "next-intl/server";
 import { PageHeader } from "@/components";
 import { CategoriesPieChart, ExpenseTypeLineChart } from "@/components/dashboard";
 import { ValueCard } from "@/components/dashboard/ValueCard";
+import { getTotalMoney } from "@/repository/getTotalMoney";
 import { LocaleParams } from "@/utils/params";
 import { createClient } from "@/utils/supabase/server";
 import { ExpenseTypes } from "@/utils/types";
@@ -17,6 +18,7 @@ export default async function Dashboard({ params: { locale } }: LocaleParams) {
   const { data: currentExpenses } = await supabase.rpc("get_this_month_expenses", { expense_type: "expense" });
   const { data: currentIncomes } = await supabase.rpc("get_this_month_expenses", { expense_type: "income" });
   const { data: averageExpenses } = await supabase.rpc("get_avg_expenses_past_six_months");
+  const totalMoney = await getTotalMoney();
 
   return (
     <NextIntlClientProvider messages={pick(messages, "Dashboard")}>
@@ -25,7 +27,7 @@ export default async function Dashboard({ params: { locale } }: LocaleParams) {
         <div className="grid grid-cols-2 gap-2">
           <ValueCard value={-Number(currentExpenses)} label={t("currentExpenses")} />
           <ValueCard value={-Number(averageExpenses)} label={t("averageExpenses")} />
-          <ValueCard value={0} label={t("totalMoney")} />
+          <ValueCard value={totalMoney} label={t("totalMoney")} />
           <ValueCard value={Number(currentIncomes) - Number(currentExpenses)} label={t("currentNetProfit")} />
         </div>
         <ExpenseTypeLineChart />
---
+++ src/repository/getTotalMoney.ts
@@ -0,0 +1,17 @@
+import { groupBy, sum, sumBy } from "lodash";
+
+import { getAccounts } from "@/repository/getAccounts";
+import { createClient } from "@/utils/supabase/server";
+
+export async function getTotalMoney() {
+  const supabase = createClient();
+  const { data: accounts } = await getAccounts();
+  const { data: expensesPerAccount } = await supabase.rpc("get_expenses_per_accounts");
+  const expensesByAccount = groupBy(expensesPerAccount, "account");
+  const totalMoneyForAccounts = Object.values(expensesByAccount).map((accountExpenses) => {
+    const incomes = accountExpenses.find((expense) => expense.type === "income")?.sum || 0;
+    const expenses = accountExpenses.find((expense) => expense.type === "expense")?.sum || 0;
+    return incomes - expenses;
+  });
+  return sum(totalMoneyForAccounts) + sumBy(accounts, "initialBalance");
+}
