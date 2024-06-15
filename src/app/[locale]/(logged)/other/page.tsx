651254967b894b6dfa89b1cbe59debd1b75f37ff
Marcin Jaborski <marcin.jaborski@gmail.com>
2024-06-15 12:16:12 +0200

    Add language selection

--- src/app/[locale]/(logged)/other/page.tsx
+++ src/app/[locale]/(logged)/other/page.tsx
@@ -3,9 +3,12 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getTranslations } from "next-intl/server";
 import { FaHandHoldingUsd } from "react-icons/fa";
 import { LuDatabaseBackup, LuFolders, LuImport, LuWallet } from "react-icons/lu";
+import Flag from "react-world-flags";
 
 import { OtherLink, PageHeader } from "@/components";
 import { LogoutLink } from "@/components/other/LogoutLink";
+import { localeFlags, locales } from "@/config";
+import { Link } from "@/navigation";
 import { LocaleParams } from "@/utils/params";
 
 export default async function Other({ params: { locale } }: LocaleParams) {
@@ -16,6 +19,16 @@ export default async function Other({ params: { locale } }: LocaleParams) {
     <NextIntlClientProvider messages={pick(messages, "Other")}>
       <div className="flex w-full flex-col items-center self-stretch">
         <PageHeader title={t("title")} />
+        <label className="flex flex-row items-center gap-2">
+          <span>{t("language")}</span>
+          {locales.map((code) => (
+            <Link href="/other" locale={code} key={code}>
+              <button className="btn" type="button" aria-label={code}>
+                <Flag code={localeFlags[code]} className="w-8" />
+              </button>
+            </Link>
+          ))}
+        </label>
         <div className="grid h-full items-center">
           <div className="mt-3 grid grid-cols-3 place-items-center gap-5">
             <OtherLink title={t("accounts")} Icon={LuWallet} href="/accounts" />
--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,11 @@ import { Pathnames } from "next-intl/navigation";
 export const locales = ["en", "pl"] as const;
 export type Locale = (typeof locales)[number];
 
+export const localeFlags: Record<Locale, string> = {
+  en: "gb",
+  pl: "pl",
+};
+
 export const pathnames = {
   "/": "/",
   "/error": "/error",
