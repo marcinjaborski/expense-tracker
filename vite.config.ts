f9df4e06391a3932371522dca8f2d9772727c9a2
Marcin Jaborski <marcin.jaborski@gmail.com>
2025-02-04 21:03:45 +0100

    Add PWA capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import * as path from "node:path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico", "apple-icon.png"],
+      manifest: {
+        name: "Expense Tracker",
+        short_name: "Expenses",
+        description: "Application to monitor and track expenses",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@src": path.resolve(__dirname, "./src"),
